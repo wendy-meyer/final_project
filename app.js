2b356dd301f91cadc231ed68526f9cba8328b411
wendy-meyer <wendy.meyer.wm@gmail.com>
2020-07-14 20:01:56 -0500

    adding table to visuals

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// const userSearch = d3.select("input-group");
-// console.log(userSearch);
+var userSearch = d3.select("input-group");
+console.log(userSearch);
 // const userSearch = dropdownMenu.property("value");
-
-filterit('chelsea');
+var tbody = d3.select("tbody");
+filterit('Chelsea');
 
 d3.select(".input-group").on("click", getData);
 
@@ -15,42 +15,61 @@ function getData() {
 }
 
 function filterit(userSearch) {
-    Plotly.d3.csv("testing_data.csv", (err, rows) => {
-
-        var tweets = rows.filter(r => ((r.stemmed_tweet).includes(userSearch) === true));
+    Plotly.d3.csv("testing_data2.csv", (err, rows) => {
+        console.log(userSearch);
+        //Filters the tweets that contain the word the user put in the seach bar.
+        var tweets = rows.filter(r => ((r.tidy_tweet).includes(userSearch) === true));
         var dict = [];
         var obj = {};
+        console.log(tweets);
 
-        tweets.forEach(function(message, i, arr){
+        //NEED Enter a 'table summary' that shows the top 5 results from the filtered tweets.
+        // Complete the event handler function for the form
+            function runEnter(tweets) {
+                tbody.html("");
+                //Show the table 
+                tweets.forEach((tweet) => {
+                    var row = tbody.append("tr");
+                    Object.entries(tweet).forEach(([key, value]) => {
+                    var cell = row.append("td");
+                    cell.text(value);
+                    });
+                });
+            }
+            runEnter(tweets);
+        //NEED Enter a dictionary for counting the number of tweets for each sentiment. 
 
-            var text = arr[i]['stemmed_tweet'] 
-            text.split(" ").forEach(function(el, i, arr) {
-            obj[el] = obj[el] ? ++obj[el] : 1;
+        //Walks through the filtered tweets and counts how many times each word was used across all tweets.
+        tweets.forEach(function (message, i, arr) {
+            var text = arr[i]['tidy_tweet']
+            text.split(" ").forEach(function (el, i, arr) {
+                obj[el] = obj[el] ? ++obj[el] : 1;
+            })
         })
-          })
-
-        Object.entries(obj).forEach(function([key, value]){
-        console.log(`${key} ${value}`)
-        dict.push({
-            x: key,
-            value: value
+        //Puts the results into a dictionary format that anychart can read to make the word cloud
+        Object.entries(obj).forEach(function ([key, value]) {
+            console.log(`${key} ${value}`)
+            dict.push({
+                x: key,
+                value: value
+            })
         })
-        })
-        
-        anychart.onDocumentReady(function() {
-        // create a tag (word) cloud chart
-        var chart = anychart.tagCloud(dict);
-        // set a chart title
-        chart.title('Most common used words')
-        // set an array of angles at which the words will be laid out
-        chart.angles([0])
-        chart.container("cloud");
-        // configure the visual settings of the chart
-        chart.normal().fill("#1fadad");
-        chart.hovered().fill("#93bfec");
-        chart.selected().fill("#1f66ad");
-        chart.normal().fontWeight(600);
-        chart.draw();
+        //Makes the word cloud, NEED to add in a condition for the color based on the sentiment type (waiting on csv)
+        anychart.onDocumentReady(function () {
+            // create a tag (word) cloud chart
+            var chart = anychart.tagCloud(dict);
+            // set a chart title
+            chart.title('Most common used words')
+            // set an array of angles at which the words will be laid out
+            chart.angles([0])
+            chart.container("cloud");
+            // configure the visual settings of the chart
+            chart.normal().fill("#1fadad");
+            chart.hovered().fill("#93bfec");
+            chart.selected().fill("#1f66ad");
+            chart.normal().fontWeight(600);
+            chart.draw();
         });
-    })}
+    })
+}
 
